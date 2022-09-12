8e5e75df0c89afc438964b40a39a337d48c3f539
Claudia Reyna <reynah.claudiae@live.com>
2022-09-11 20:58:56 -0500

    u

--- src/components/FechaLugar.js
+++ src/components/FechaLugar.js
@@ -20,7 +20,7 @@ const FechaLugar = () => {
                <div className='pb-3 fs-4 text-center' style={{color:"#d2b49a"}}>
                 <SvgIcon component={ChurchRoundedIcon} fontSize='large'/> </div>
 
-                <h3 className='pb-2' style={{fontFamily:"'Quattrocento', cursive", color: "#d2b49a" , fontSize:"24px"}}>La Mansión del Fundador</h3>
+                <h3 className='pb-2' style={{fontFamily:"'Quattrocento', cursive", color: "#bd9045" , fontSize:"24px"}}>La Mansión del Fundador</h3>
                 <p>Huasacache s/n Vía Paisajista<br/>Hunter, Arequipa - Perú</p>
                 <div className='d-flex justify-content-center pt-4'>
                       <iframe title='La Mansión del fundador' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3826.4091980724806!2d-71.56549393527062!3d-16.454805088643727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9143b552ff9c98bb%3A0xc12a9f5af25c81ae!2sLa%20Mansi%C3%B3n%20del%20Fundador%2C%20561%2038%2C%20Arequipa%2004011!5e0!3m2!1ses-419!2spe!4v1654104549083!5m2!1ses-419!2spe" width="400" height="250" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
